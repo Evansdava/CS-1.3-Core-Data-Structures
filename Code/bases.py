55ee07089867d6b03b823583d6f9560bea7286b7
David Evans <evansdav.a@gmail.com>
2020-01-29 11:50:55 -0800

    Adjusted formatting

--- Code/bases.py
+++ Code/bases.py
@@ -19,7 +19,8 @@ def decode(digits, base):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
 
-    printable = string.digits + string.ascii_lowercase
+    printable = string.digits + string.ascii_uppercase
+    digits = digits.upper()
     """
     Check the length of the string
     Loop from right to left
@@ -47,7 +48,7 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    printable = string.digits + string.ascii_lowercase
+    printable = string.digits + string.ascii_uppercase
     """
     While the number can be divided by the base:
         Divide the number by the base
@@ -92,7 +93,7 @@ def main():
     import sys
     args = sys.argv[1:]  # Ignore script file name
     if len(args) == 3:
-        digits = args[0]
+        digits = args[0].upper()
         base1 = int(args[1])
         base2 = int(args[2])
         # Convert given digits between bases
